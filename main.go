c748517338dafe376a8f971a4338cbf7fd286d8b
Phillip Schichtel <phillip.public@schich.tel>
2017-05-11 22:48:11 +0200

    implement additional pages, authentication and proper db access

--- main.go
+++ main.go
@@ -14,10 +14,19 @@ import (
 	"database/sql"
 	"math/rand"
 	"time"
+	"strings"
 )
 
-func redirectToRegister(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/register", http.StatusFound)
+const AuthCookieName = "auth"
+
+func handleIndex(pass string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if checkPasswordAgainstCookie(r, pass) {
+			http.Redirect(w, r, "/list", http.StatusFound)
+		} else {
+			http.Redirect(w, r, "/login", http.StatusFound)
+		}
+	}
 }
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
@@ -32,7 +41,7 @@ func GenerateString(n int) string {
 func handleRegisterPage(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	if r.Method == "POST" {
 		r.ParseForm()
-		username := r.PostForm.Get("username")
+		username := strings.TrimSpace(r.PostForm.Get("username"))
 		password := r.PostForm.Get("password")
 
 		if len(username) == 0 || len(password) == 0 {
@@ -45,17 +54,18 @@ func handleRegisterPage(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 				http.Error(w, "Crypt: " + err.Error(), http.StatusInternalServerError)
 				return
 			}
-			_, err = db.Exec("INSERT INTO accounts (username, password) VALUES (?, ?)", username, hashed)
+			_, err = db.Exec("INSERT INTO radcheck (username, attribute, op, value) VALUES (?, 'Crypt-Password', ':=', ?)", username, hashed)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
-			fmt.Fprintf(w, "Successfully registered %s!", username + ":" + hashed)
+
+			http.Redirect(w, r, "/list", http.StatusFound)
 
 		}
 
 	} else {
-		content, err := ioutil.ReadFile("register.html");
+		content, err := ioutil.ReadFile("register.html")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		} else {
@@ -64,19 +74,112 @@ func handleRegisterPage(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	}
 }
 
-func makeHandler(fn func(w http.ResponseWriter, r *http.Request, db *sql.DB), db *sql.DB) http.HandlerFunc {
+func handleListPage(w http.ResponseWriter, _ *http.Request, db *sql.DB) {
+	var result, err = db.Query("SELECT DISTINCT username FROM radcheck")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	} else {
+		fmt.Fprint(w, "<h1>Available accounts</h1>\n")
+		fmt.Fprint(w, "<p><a href=\"/register\">Register new</a></p>")
+		fmt.Fprint(w, "<ul>\n")
+		for result.Next() {
+			var username string
+			result.Scan(&username)
+			fmt.Fprintf(w, "<li>%s <a href=\"/delete?user=%s\">delete</a></li>", username, username)
+		}
+		fmt.Fprint(w, "</ul>\n")
+	}
+}
+
+func handleDeletePage(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	var user = r.URL.Query().Get("user")
+	if len(user) == 0 {
+		http.Error(w, "No user given!", http.StatusBadRequest)
+	} else {
+		var result, err = db.Exec("DELETE FROM radcheck WHERE username = ?", user)
+		var affectedRows, _ = result.RowsAffected()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		} else if affectedRows == 0 {
+			http.Error(w, fmt.Sprintf("User %s not found!", user), http.StatusNotFound)
+		} else {
+			http.Redirect(w, r, "/list", http.StatusFound)
+		}
+	}
+}
+
+func handleLoginPage(pass string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			r.ParseForm()
+			password := r.PostForm.Get("password")
+
+			if password == pass {
+				var cookie = http.Cookie{Name:AuthCookieName, Value:password}
+				http.SetCookie(w, &cookie)
+				http.Redirect(w, r, "/", http.StatusFound)
+			} else {
+				http.Error(w, "Invalid password!", http.StatusBadRequest)
+			}
+
+		} else {
+			content, err := ioutil.ReadFile("login.html")
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			} else {
+				fmt.Fprintf(w, "%s", content)
+			}
+		}
+	}
+}
+
+func withDB(fn func(w http.ResponseWriter, r *http.Request, db *sql.DB), db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fn(w, r, db)
+	}
+}
+
+func withLog(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("Access: %s %s\n", r.Method, r.URL.Path)
+		fn(w, r)
+	}
+}
+
+func authenticationWrapper(pass string) func(http.HandlerFunc) http.HandlerFunc {
+	return func(fn http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			if checkPasswordAgainstCookie(r, pass) {
+				fn(w, r)
+			} else {
+				http.Redirect(w, r, "/login", http.StatusFound)
+			}
+		}
+	}
+}
+
+func checkPasswordAgainstCookie(r *http.Request, pass string) bool {
+	var cookie, err = r.Cookie(AuthCookieName)
+	if err != nil {
+		return false
+	} else {
+		return cookie.Value == pass
 	}
 }
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	var dbPath string
+	var dsn string
 	var driver string
+	var listenPort int
+	var listenAddr string
+	var password string
 	flag.StringVar(&driver, "driver", "sqlite3", "Provides the database driver")
-	flag.StringVar(&dbPath, "dsn", "accounts.db", "Provides the database dsn")
+	flag.StringVar(&dsn, "dsn", "accounts.db", "Provides the database dsn")
+	flag.IntVar(&listenPort, "listen-port", 80, "Provides the listening port")
+	flag.StringVar(&listenAddr, "listen-address", "0.0.0.0", "Provides the listening address")
+	flag.StringVar(&password, "password", "sicher", "Provides")
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
 		flag.PrintDefaults()
@@ -86,11 +189,13 @@ func main() {
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
 
-	db, err := sql.Open(driver, dbPath)
-	if err != nil {
+	db, sqlErr := sql.Open(driver, dsn)
+	if sqlErr != nil {
 		fmt.Println("Failed to connect to the database!")
-		fmt.Println(err.Error())
+		fmt.Println(sqlErr.Error())
 		os.Exit(1)
+	} else {
+		fmt.Printf("Successfully connected to the database: %s\n", dsn)
 	}
 
 	go func() {
@@ -100,7 +205,19 @@ func main() {
 		os.Exit(0)
 	}()
 
-	http.HandleFunc("/", redirectToRegister)
-	http.HandleFunc("/register", makeHandler(handleRegisterPage, db))
-	http.ListenAndServe(":9001", nil)
+	var listenOn = fmt.Sprintf("%s:%d", listenAddr, listenPort)
+	fmt.Printf("Listening on %s\n", listenOn)
+
+	var withAuth = authenticationWrapper(password)
+
+	http.HandleFunc("/", withLog(handleIndex(password)))
+	http.HandleFunc("/login", withLog(handleLoginPage(password)))
+	http.HandleFunc("/register", withAuth(withLog(withDB(handleRegisterPage, db))))
+	http.HandleFunc("/list", withAuth(withLog(withDB(handleListPage, db))))
+	http.HandleFunc("/delete", withAuth(withLog(withDB(handleDeletePage, db))))
+	var listenErr = http.ListenAndServe(listenOn, nil)
+	if listenErr != nil {
+		fmt.Println(sqlErr.Error())
+		os.Exit(1)
+	}
 }
